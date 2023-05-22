fd0f3c3f59aa218a2a9c22f1adbb8ab9f1ddaf22
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2023-05-22 12:08:27 +0600

    update js file

--- js/custom.js
+++ js/custom.js
@@ -3,22 +3,7 @@ $(document).ready(function () {
   //Data Table
   $('#example').DataTable();
 
-  //  setup from 
-  $('.show-content').click(function () {
-    $('.overlay').fadeIn(500);
-    $('.content').animate({ right: 0 }, 500);
-  });
-  $('.close-content').click(function () {
-    $('.overlay').fadeOut(500);
-    $('.content').animate({ right: -500 }, 500);
-  });
 
-  $(".hide-select-client").click(function () {
-    $(".Slelect-Client").hide()
-  })
-  $(".show-select-client").click(function () {
-    $(".Slelect-Client").show()
-  })
 
   // All Project show and hide table ans image foulder
   $(".icon-table").click(function () {
